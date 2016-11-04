046a1e581b015b326f864352431382a475a4eca1
Vincent Rialland <vincent.rialland@net-ng.com>
2016-11-04 12:27:09 +0100

    Refactor grid and Led

--- scripts/led.gd
+++ scripts/led.gd
@@ -1,26 +1,36 @@
 
 extends Node2D
 
-export var color = Color(0, 0, 0, 255)
-export var size = 24
-export var switched_on = false
+export var color = Color(0, 255, 0, 255)
+export var size = 26
 
 func _on_draw():
-	if not self.switched_on:
-		color = Color(0, 0, 0, 255)
-	else:
-		color = self.color
 	var radius = self.size / 2
-	self.draw_circle(Vector2(radius, radius), radius, color)
+	self.draw_circle(Vector2(radius, radius), radius, self.color)
 
-func set_random_color():
-	self.color = Color(randi() * 255, randi() * 255, randi() * 255, 255)
+func set_color(color):
+	self.color.r = color.r
+	self.color.g = color.g
+	self.color.b = color.b
+	self.update()
 
-func set_random_state():
-	if randi() * 2 > 0:
-		self.switched_on = true
-	else:
-		self.switched_on = false
+func set_color_with_alpha(color):
+	self.color = color
+	self.update()
 
 func toggle():
-	self.switched_on = not self.switched_on
+	if self.color.a > 0:
+		self.off()
+	else:
+		self.on()
+
+func on():
+	self.color.a = 255
+	self.update()
+
+func off():
+	self.color.a = 60
+	self.update()
+
+func copy(other):
+	self.set_color_with_alpha(other.color)
--- scripts/ledmatrix.gd
+++ scripts/ledmatrix.gd
@@ -4,29 +4,53 @@ extends Node2D
 export var rows = 16
 export var cols = 32
 export var updates_per_minute = 60
-var cell_size
 
 onready var Led = preload('res://scenes/Led.tscn')
-var led_grid = []
+
+var _grid = []
 var elapsed = 0
+var cell_size
 var window_size = Vector2(0, 0)
 
+func _get_index_from_coords(col, row):
+	return row * self.cols + col
+
+func _get_coords_from_index(i):
+	return [i % self.cols, i / self.cols]
+
+func set_led_at(col, row, led):
+	self._grid[self._get_index_from_coords(col, row)] = led
+
+func get_led_at(col, row):
+	return self._grid[self._get_index_from_coords(col, row)]
+
 func _ready():
+	var width = int(self.window_size.x)
+	var height = int(self.window_size.y)
+
 	var size = OS.get_window_size()
-	self.window_size = Vector2(size[0], size[1])
-	var led_width = self.window_size.x / self.cols
-	var led_height = self.window_size.y / self.rows
+	self.window_size = Vector2(int(size[0]), int(size[1]))
+	var cell_width = self.window_size.x / self.cols
+	var cell_height = self.window_size.y / self.rows
+	var delta_x = null
+	var delta_y = null
 
 	# Init led grid
-	for col in range(cols):
-		var col_grid = []
-		for row in range(rows):
-			var led = Led.instance()
-			led.set_pos(Vector2(led_width * col, led_height * row))
-			led.set_random_color()
-			col_grid.append(led)
-			self.add_child(led)
-		self.led_grid.append(col_grid)
+	for i in range(self.cols * self.rows):
+		# col, row
+		var coords = self._get_coords_from_index(i)
+		var led = Led.instance()
+		if delta_x == null:
+			delta_x = (cell_width - led.size) / 2
+			delta_y = (cell_height - led.size) / 2
+
+		self._grid.append(led)
+		led.set_pos(Vector2(cell_width * coords[0] + delta_x, cell_height * coords[1] + delta_y))
+		self.add_child(led)
+
+	for i in range(self.rows):
+		self.get_led_at(i, i).on()
+
 	self.set_process(true)
 
 func _on_draw():
@@ -37,50 +61,42 @@ func _process(delta):
 	self.elapsed += delta
 	if self.elapsed > 60.0 / self.updates_per_minute:
 		self.elapsed = 0
-		self.animate()
+		self.horizontal_scroll()
 
-func animate():
-	self.horizontal_scroll(true)
-
-func get_led_at(col, row):
-	return self.led_grid[col][row]
-
-func set_led_state(col, row, color, switched_on=true):
+func set_led_color(col, row, color):
 	var led = self.get_led_at(col, row)
-	led.color = color
-	led.switched_on = switched_on
-	led.update()
-		
+	led.set_color(color)
+
 func horizontal_scroll(left_to_right=false):
 	var buffer = []
 	var col = 0
 	var row = 0
-	
+
 	if left_to_right:
-		col = self.cols - 1 
-	
+		col = self.cols - 1
+
 	# get content of first col (right to left) or last one in memory
 	for row in range(self.rows):
 		var led = self.get_led_at(col, row)
-		buffer.append([led.color, led.switched_on])
-		
+		buffer.append(led.color)
+
 	if left_to_right:
 		# copy col - 1 to col
 		for col in range(self.cols - 1, 0, -1):
 			for row in range(self.rows):
 				var src = self.get_led_at(col - 1, row)
-				self.set_led_state(col, row, src.color, src.switched_on)
+				self.get_led_at(col, row).copy(src)
 	else:
 		# copy col + 1 to col
 		for col in range(self.cols - 1):
 			for row in range(self.rows):
 				var src = self.get_led_at(col + 1, row)
-				self.set_led_state(col, row, src.color, src.switched_on)
-				
+				self.get_led_at(col, row).copy(src)
+
 	# copy 1st col to last one (right to left) or first one
 	if left_to_right:
 		col = 0
 	else:
 		col = self.cols - 1
 	for row in range(self.rows):
-		self.set_led_state(col, row, buffer[row][0], buffer[row][1])
+		self.get_led_at(col, row).set_color_with_alpha(buffer[row])
